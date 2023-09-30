4ef13231d01bd265b0944a80cf6e97c4d2cdea1b
adnoryuGit <adnansulaiman2014@gmail.com>
2023-09-30 20:45:40 +0400

    tes

--- src/app/view-client/view-client.component.ts
+++ src/app/view-client/view-client.component.ts
@@ -7,35 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-client.component.css']
 })
 export class ViewClientComponent implements OnInit{
-clients:any[]=[];
+  clients:any[]=[];
 
-message:string='';
-  constructor(private http:HttpClient) { }
-
-
-  ngOnInit(): void {
-  this.fetchclients
-  }
-
-
-  fetchclients(){
-    this.http.get('http://localhost:3000/viewClients')
-      .subscribe((response:any)=>
-    {this.clients=response},
-    (error)=>{console.error('Error fetching the client',error);}
-  );
-  }
-  deleteClient(id:number){
-    if(confirm('Are you sure you want to delete this client?')){
-      this.http.delete('http://localhost:3000/deleteclient/'+id)
-      .subscribe((response:any)=>
-    {this.message=response.message;
+  message:string='';
+    constructor(private http:HttpClient) { }
+  
+  
+    ngOnInit(): void {
+    this.fetchclients();
+    }
+  
+  
+    fetchclients(){
+      this.http.get('http://localhost:3000/viewClients')
+        .subscribe((response:any)=>
+      {this.clients=response},
+      (error)=>{console.error('Error fetching the client',error);}
+    );
+    }
+    deleteClient(id:number){
+      if(confirm('Are you sure you want to delete this client?')){
+        this.http.delete('http://localhost:3000/deleteclient/'+id)
+        .subscribe((response:any)=>
+      {this.message=response.message;
+       
+        this.fetchclients();},
      
-      this.fetchclients();},
-   
-    (error)=>{console.error('Error deleting the client',error);}
-  );
-}
-
+      (error)=>{console.error('Error deleting the client',error);}
+    );
   }
+  
+    }
 }
